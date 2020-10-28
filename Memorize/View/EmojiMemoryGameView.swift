8e36ae2a2422ac64eb9f4bd9750f1fa8d176f1a5
Pierre Younes <pyounes@gmail.com>
2020-10-28 16:13:18 +0200

    helper function for MV

--- Memorize/View/EmojiMemoryGameView.swift
+++ Memorize/View/EmojiMemoryGameView.swift
@@ -20,25 +20,53 @@ struct EmojiMemoryGameView: View {
         }
     .padding()
     .foregroundColor(Color.orange)
-    .font(Font.largeTitle)
     }
 }
 
 struct CardView: View {
+    
     var card: MemoryGame<String>.Card
     
     var body: some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text(card.content)
-            } else {
-                RoundedRectangle(cornerRadius: 10.0).fill()
-            }
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
         }
     }
+    
+    func body(for size: CGSize) -> some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            }
+        }
+        .font(Font.system(size: fontSize(for: size)))
+    }
+    
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width, size.height) * 0.75
+    }
+    
+    // MARK: - Drawing Constants
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
